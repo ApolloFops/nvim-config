74abd52e0ec843e4ab208f1622381007cc221ea0
Apollo Nargang <CoffeeCoder1@outlook.com>
2026-07-07 20:43:42 -0400

    Add a markdown table formatting extension

--- lua/plugins/lsp/markdown.lua
+++ lua/plugins/lsp/markdown.lua
@@ -6,4 +6,21 @@ return {
 			vim.fn['mkdp#util#install']()
 		end,
 	},
+	{
+		'Kicamon/markdown-table-mode.nvim',
+		ft = { 'markdown' },
+		config = function()
+			require('markdown-table-mode').setup({
+				filetype = {
+					'*.md',
+				},
+				options = {
+					insert = true, -- when typing "|"
+					insert_leave = true, -- when leaving insert
+					pad_separator_line = true, -- add space in separator line
+					alig_style = 'default', -- default, left, center, right
+				},
+			})
+		end
+	}
 }
